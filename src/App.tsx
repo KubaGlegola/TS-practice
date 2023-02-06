3bb1280537f2fc81c4b307301b466329d86f411c
KubaGlegola <kubaglegola@gmail.com>
2023-02-06 12:36:20 +0100

    working with props and refs

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
+import Todo from "./models/todo";
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHanlder = (todoText: string) => {};
+
   return (
     <div>
-      <Todos />
+      <NewTodo onAddTodo={addTodoHanlder} />
+      <Todos items={todos} />
     </div>
   );
 }
